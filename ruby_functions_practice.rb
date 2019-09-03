72cbf85d133b5393ad45d05a705f178c9225cadb
james-roberts-web <jamesroberts@xponorth.co.uk>
2019-09-03 18:23:03 +0100

    second commit

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,74 @@
+def return_10
+  return 10
+end
+
+def add(first_number, second_number)
+  return first_number + second_number
+end
+
+def subtract(first_number, second_number)
+  return first_number - second_number
+end
+
+def multiply(first_number, second_number)
+  return first_number * second_number
+end
+
+def divide(first_number, second_number)
+  return first_number / second_number
+end
+
+def length_of_string(string)
+  return string.length
+end
+
+def join_string(string_1, string_2)
+  return string_1 + string_2
+end
+
+def add_string_as_number(string_1, string_2)
+  return string_1.to_i + string_2.to_i
+end
+
+#
+# def number_to_full_name(number)
+#   case month
+#   when number
+#       puts number_to_full_month_name
+#   end
+# end
+#
+# def number_to_full_name(month)
+#   return number_to_full_month_name puts "#{month}"
+# end
+#
+#
+#
+#   case intiger
+#   when 1
+#     return "one"
+#   end
+#
+#   def number_to_full_month_name(number)
+#     case intiger
+#     when "one"
+#       puts "January"
+#     end
+#   end
+#
+#
+
+def number_to_full_name()
+  case number_to_full_month_name
+  when "One"
+    return "January"
+
+
+
+end
+
+
+#
+#
+#   return number_to_full_month_name()
+# end
